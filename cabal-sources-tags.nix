6250cf74c3b3210905fec2b18c9334853448072c
Sergei Dolgov <dolgovs@gmail.com>
2020-06-16 05:58:34 +0200

    Format

--- cabal-sources-tags.nix
+++ cabal-sources-tags.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
-  pkgs.writeShellScriptBin "cabal-sources-tags" ''
-    nix-build --out-link dependencies --arg cabalProject ./. ${./.}
-    nix-shell -p haskellPackages.fast-tags --run "fast-tags -R ."
-  ''
+pkgs.writeShellScriptBin "cabal-sources-tags" ''
+  nix-build --out-link dependencies --arg cabalProject ./. ${./.}
+  nix-shell -p haskellPackages.fast-tags --run "fast-tags -R ."
+''
